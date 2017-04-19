4b9fb9b82db9a03c765bb8fd68ca9f652ebd5e97
maeda-m <maeda-m@matsukei.co.jp>
2017-04-19 14:40:55 +0900

    チケットの通知メールが届くユーザーをサイドバーに表示

--- app/hooks/issue_view_hook.rb
+++ app/hooks/issue_view_hook.rb
@@ -1,5 +1,6 @@
 module MatsukeiRedmine
   class IssueViewHook < Redmine::Hook::ViewListener
     render_on :view_issues_form_details_bottom, partial: "issues/default_watcher"
+    render_on :view_issues_sidebar_queries_bottom, partial: "issues/notified_users"
   end
 end
